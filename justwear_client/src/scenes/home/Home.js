b327f5cb7971d4098448adaa8dfd199dd5138cb5
Abhishek Yadav <abhiyd2401@gmail.com>
2023-01-25 08:12:10 +0530

    in progress: completing the home page

--- justwear_client/src/scenes/home/Home.js
+++ justwear_client/src/scenes/home/Home.js
@@ -1,19 +1,30 @@
 import { Image } from "@mui/icons-material";
-import { Container } from "@mui/material";
+import { Box, Button, IconButton, Typography, Container } from "@mui/material";
 import Carousel from "react-material-ui-carousel";
 import autumn from "../../assets/autumn_home.svg";
 import casual from "../../assets/casual.svg";
+import outfit from "../../assets/women_outfit.svg";
+import { theme } from "../../theme.js";
 
 const Home = () => {
-  const images = [autumn, casual];
+  const images = [autumn, casual, outfit];
   return (
-    <>
-      <Carousel>
-        {images.map((img, i) => (
-          <img src={img} alt="images" />
-        ))}
-      </Carousel>
-    </>
+    <Container width="100%">
+      <Container>
+        <Carousel>
+          {images.map((img, i) => (
+            <img src={img} alt="images" style={{ width: "100%" }} />
+          ))}
+        </Carousel>
+      </Container>
+      <Container>
+        <Typography
+          sx={{ fontFamily: theme.typography.fontFamily, fontSize: "2rem" }}
+        >
+          Collections
+        </Typography>
+      </Container>
+    </Container>
   );
 };
 
