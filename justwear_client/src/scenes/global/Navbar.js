25a3f8d5ff3596c5fa2f72cbde0198f9f8332e19
Abhishek Yadav <abhiyd2401@gmail.com>
2023-01-26 16:40:52 +0530

    added the onclick prop to cart icon

--- justwear_client/src/scenes/global/Navbar.js
+++ justwear_client/src/scenes/global/Navbar.js
@@ -1,5 +1,4 @@
 import "./navbar.css";
-import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import {
   SearchOutlined,
@@ -7,12 +6,18 @@ import {
   ShoppingBagOutlined,
   PersonOutline,
 } from "@mui/icons-material";
+import SearchBar from "material-ui-search-bar";
 import { Box, IconButton, Typography } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
+import { setIsCartOpen } from "../../state";
+import { useState } from "react";
 
 function Navbar() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart.cart);
+  const category = ["mens", "womens", "kids", "sports"];
+  const [expandSearch, setExpandSearch] = useState(false);
 
   return (
     <Box
@@ -34,18 +39,26 @@ function Navbar() {
         alignItems="center"
         gap="80px"
       >
-        <Typography variant="h4" sx={{ cursor: "pointer" }}>
-          Mens
-        </Typography>
-        <Typography variant="h4" sx={{ cursor: "pointer" }}>
-          Womens
-        </Typography>
-        <Typography variant="h4" sx={{ cursor: "pointer" }}>
-          Kids
-        </Typography>
-        <Typography variant="h4" sx={{ cursor: "pointer" }}>
-          Sports
-        </Typography>
+        <Link to={`/shop/${category[0]}`}>
+          <Typography variant="h4" sx={{ cursor: "pointer" }}>
+            Mens
+          </Typography>
+        </Link>
+        <Link to={`/shop/${category[1]}`}>
+          <Typography variant="h4" sx={{ cursor: "pointer" }}>
+            Womens
+          </Typography>
+        </Link>
+        <Link to={`/shop/${category[2]}`}>
+          <Typography variant="h4" sx={{ cursor: "pointer" }}>
+            Kids
+          </Typography>
+        </Link>
+        <Link to={`/shop/${category[3]}`}>
+          <Typography variant="h4" sx={{ cursor: "pointer" }}>
+            Sports
+          </Typography>
+        </Link>
 
         <Box
           onClick={() => navigate("/")}
@@ -66,12 +79,14 @@ function Navbar() {
           Contact
         </Typography>
 
-        <IconButton>
+        <IconButton onClick={() => setExpandSearch(true)}>
           <SearchOutlined />
         </IconButton>
-        <IconButton>
+
+        <IconButton onClick={() => dispatch(setIsCartOpen({}))}>
           <ShoppingBagOutlined />
         </IconButton>
+
         <IconButton>
           <PersonOutline />
         </IconButton>
