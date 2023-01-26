06f3b5d77dc2000b1f37958e926d2c0c54b34b23
Abhishek Yadav <abhiyd2401@gmail.com>
2023-01-26 16:40:26 +0530

    implemented cart feature

--- justwear_client/src/scenes/global/CartMenu.js
+++ justwear_client/src/scenes/global/CartMenu.js
@@ -4,7 +4,6 @@ import CloseIcon from "@mui/icons-material/Close";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 import styled from "@emotion/styled";
-import { shades } from "../../theme";
 import {
   decreaseCount,
   increaseCount,
@@ -15,9 +14,9 @@ import {
 import { useNavigate } from "react-router-dom";
 
 const FlexBox = styled(Box)`
-  display: flex,
-  justify-content: space-between,
-  align-items: center
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const CartMenu = () => {
@@ -28,12 +27,13 @@ const CartMenu = () => {
 
   const totalPrice = cart.reduce((total, item) => {
     return total + item.count * item.attributes.price;
-  });
+  }, 0);
+
   return (
     <Box
       display={isCartOpen ? "block" : "none"}
       backgroundColor="rgba(0, 0, 0, 0.4)"
-        position="fixed"
+      position="fixed"
       zIndex={10}
       width="100%"
       height="100%"
@@ -52,7 +52,7 @@ const CartMenu = () => {
       >
         <Box padding="30px" overflow="auto" height="100%">
           {/* Header */}
-          <FlexBox mb="15px">
+          <FlexBox mb="1px">
             <Typography variant="h3">Shopping Bag ({cart.length}) </Typography>
             <IconButton onClick={() => dispatch(setIsCartOpen({}))}>
               <CloseIcon />
@@ -62,8 +62,87 @@ const CartMenu = () => {
           {/* CART LIST */}
           <Box>
             {cart.map((item) => (
-              <Box></Box>
+              <Box key={`${item.attributes.name}-${item.id}`}>
+                <FlexBox p="15px 0">
+                  <Box flex="1 1 40%">
+                    <img
+                      alt={item?.name}
+                      width="123px"
+                      height="164px"
+                      src={`http://localhost:1337{item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+                    />
+                  </Box>
+                  <Box flex="1 1 40%">
+                    <FlexBox mb="5px">
+                      <Typography fontWeight="bold">
+                        {item.attributes.name}
+                      </Typography>
+                      <IconButton
+                        onClick={() =>
+                          dispatch(removeFromCart({ id: item.id }))
+                        }
+                      >
+                        <CloseIcon />
+                      </IconButton>
+                    </FlexBox>
+                    <Typography>{item.attributes.shortDescription}</Typography>
+                    <FlexBox m="15px 0">
+                      <Box
+                        display="flex"
+                        alignItems="center"
+                        border="1.5px solid black"
+                      >
+                        <IconButton
+                          onClick={() =>
+                            dispatch(decreaseCount({ id: item.id }))
+                          }
+                        >
+                          <RemoveIcon />
+                        </IconButton>
+                        <Typography>{item.count}</Typography>
+                        <IconButton
+                          onClick={() =>
+                            dispatch(increaseCount({ id: item.id }))
+                          }
+                        >
+                          <AddIcon />
+                        </IconButton>
+                      </Box>
+                      {/* PRICE */}
+                      <Typography fontWeight="bold">
+                        ₹{item.attributes.price}
+                      </Typography>
+                    </FlexBox>
+                  </Box>
+                </FlexBox>
+                <Divider />
+              </Box>
             ))}
+          </Box>
+
+          {/* ACTIONS */}
+          <Box m="20px 0">
+            <FlexBox m="20px 0">
+              <Typography>SUBTOTAL</Typography>
+              <Typography>₹{totalPrice}</Typography>
+            </FlexBox>
+            <Button
+              sx={{
+                "&:hover": { backgroundColor: "#383838" },
+                backgroundColor: "#070707",
+                color: "white",
+                borderRadius: "0",
+                minWidth: "100%",
+                padding: "20px 40px",
+                m: "20px 0",
+              }}
+              onClick={() => {
+                navigate("/checkout");
+                dispatch(setIsCartOpen({}));
+              }}
+            >
+              Checkout
+            </Button>
           </Box>
         </Box>
       </Box>
