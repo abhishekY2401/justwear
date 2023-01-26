aa6113b19e06ae20e73225396f74c2ea31e34e19
Abhishek Yadav <abhiyd2401@gmail.com>
2023-01-26 16:46:36 +0530

    adding collections category

--- justwear_client/src/scenes/home/Home.js
+++ justwear_client/src/scenes/home/Home.js
@@ -1,10 +1,18 @@
 import { Image } from "@mui/icons-material";
-import { Box, Button, IconButton, Typography, Container } from "@mui/material";
+import {
+  Box,
+  Button,
+  IconButton,
+  Typography,
+  Container,
+  ButtonBase,
+} from "@mui/material";
 import Carousel from "react-material-ui-carousel";
 import autumn from "../../assets/autumn_home.svg";
 import casual from "../../assets/casual.svg";
 import outfit from "../../assets/women_outfit.svg";
 import { theme } from "../../theme.js";
+import Item from "../../components/Item";
 
 const Home = () => {
   const images = [autumn, casual, outfit];
@@ -13,17 +21,32 @@ const Home = () => {
       <Container>
         <Carousel>
           {images.map((img, i) => (
-            <img src={img} alt="images" style={{ width: "100%" }} />
+            <div>
+              <img src={img} alt="images" style={{ width: "100%" }} />
+              <ButtonBase>Explore More Products</ButtonBase>
+            </div>
           ))}
         </Carousel>
       </Container>
       <Container>
         <Typography
-          sx={{ fontFamily: theme.typography.fontFamily, fontSize: "2rem" }}
+          sx={{
+            fontFamily: theme.typography.fontFamily,
+            fontSize: "1.5rem",
+            letterSpacing: "-0.5px",
+            textAlign: "center",
+          }}
         >
           Collections
         </Typography>
       </Container>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      ></Box>
     </Container>
   );
 };
